8c4e1474bce38f72a012e27234efecbea430c841
CongDangLuu <congdang143@gmail.com>
2023-12-09 12:03:43 +0700

    Generate binary file

--- BuildNumber.py
+++ BuildNumber.py
@@ -5,14 +5,26 @@ Import("env")
 #
 # Custom actions when building program/firmware
 #
+def strBuildnumber(num):
+    strnum = "" 
+    if num > 999:
+        return "000"
+    
+    strnum += str((int)(num/100))
+    strnum += str((int)(num/10)%10)
+    strnum += str((int)(num%100%10))
+    return strnum
 
 def after_building(source, target, env):
+    print("run after_building")
     srcfile=source[0].get_abspath()
     #read build number
     file1 = open('BuildNum.txt','r')
     BN=int(file1.readline(2))
     file1.close()
-    dstfile=os.path.abspath(os.getcwd())+"/Target/Image_" + str(BN) + ".bin"
+    strBN = strBuildnumber(BN)
+    dstfile=os.path.abspath(os.getcwd())+"/Target/Image-" + strBN + ".bin"
+    print("File taget: "+ dstfile)
     BN +=1
     file1 = open('BuildNum.txt','w')
     file1.write(str(BN))
